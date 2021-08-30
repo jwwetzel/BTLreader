7f6b1112c6660f7956ed84be6ea236df7bb3e3ea
James Wetzel <jwwetzel@icloud.com>
2021-08-30 09:38:56 -0500

    new plots

--- BTL_Reader.py
+++ BTL_Reader.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import os
 from collections import defaultdict
 plt.rc('font',size=14)
 
@@ -21,6 +22,13 @@ pressureLevels  = []
 tempLevels      = defaultdict(list)
 currentLevels   = defaultdict(list)
 
+outputName = "Analyzed"
+
+if args.fileName.endswith(".txt"):
+    outputName = args.fileName[:-4]+"Analyzed"
+    if not os.path.isdir(outputName):
+        os.mkdir(outputName)
+
 with open(args.fileName) as dataFile:
     for line in dataFile:
         if line.startswith('Delay'):
@@ -38,34 +46,70 @@ with open(args.fileName) as dataFile:
                     channel += 1
                 else:
                     temp = float(ch.split("E")[0])*10**int(ch.split("E")[1][0:3])*1000000000
-                    print(temp)
                     currentLevels["Channel "+str(channel)].append(temp)
+                    channel += 1
                         
 #print("Delay",delay)
 #print("sipmPowerLevels",sipmPowerLevels)
 #print("asicPowerLevels",asicPowerLevels)
 #print(tempLevels)
 
-#
-if args.channel is None:
-    for chan in range(len(tempLevels)):
-        x = [int(delay)*int(item) for item in range(len(tempLevels["Channel "+str(chan)]))]
-        y = tempLevels["Channel "+str(chan)]
-        plt.title("BTL Temperature vs Time")
-        plt.xlabel("Time (Seconds)")
-        plt.ylabel("Temperature ˚C")
-        plt.plot(x, y, '.', linestyle='dashdot', label="Channel "+str(chan))
-else:
-    x = [int(delay)*int(item) for item in range(len(tempLevels["Channel "+str(args.channel)]))]
-    y = tempLevels["Channel "+str(args.channel)]
-    plt.title("BTL Temperature vs Time Channel "+str(args.channel))
-    plt.xlabel("Time (Seconds)")
-    plt.ylabel("Temperature ˚C")
-    plt.plot(x, y, '.', linestyle='dashdot', label="Channel "+str(args.channel))
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.tight_layout()
-plt.savefig(args.fileName+"_analyzed.png", bbox_inches="tight")
-plt.show()
+
+allChanFig  = plt.figure(constrained_layout=True)
+allChanFig.set_figheight(7)
+allChanFig.set_figwidth(8)
+
+allChanGS   = allChanFig.add_gridspec(4,1)
+
+allChanAx1  = allChanFig.add_subplot(allChanGS[0:3,:])
+allChanAx1.set_title('Channels 0-20 Temperature vs time')
+
+allChanAx2  = allChanFig.add_subplot(allChanGS[3:,:])
+
+for chan in range(len(tempLevels)):
+    x = [int(delay)*int(item) for item in range(len(tempLevels["Channel "+str(chan)]))]
+    y = tempLevels["Channel "+str(chan)]
+    allChanAx1.plot(x, y, '.', linestyle='dashdot', label="Channel "+str(chan))
+    allChanAx1.set_title("BTL Temperature vs Time")
+    allChanAx1.set_ylabel("Temperature ˚C")
+
+allChanAx2.set_xlabel("Time (Seconds)")
+allChanAx2.set_ylabel("Current (nA)")
+x = [int(delay)*int(item) for item in range(len(currentLevels["Channel 20"]))]
+y = currentLevels["Channel 20"]
+allChanAx2.plot(x,y,'.', linestyle='dashdot', label = "ADC 0")
+y = currentLevels["Channel 21"]
+allChanAx2.plot(x,y,'.', linestyle='dashdot', label = "ADC 1")
+
+allChanFig.legend(bbox_to_anchor=(1.0, 0.95), loc='upper left')
+allChanFig.savefig(outputName+"/"+outputName+"_All_Channels.png", bbox_inches="tight")
+
+
+
+#allChanFig, allChanAxes = plt.subplots(2,sharex=True)
+
+#singleChanFig, singleChanAxes = plt.subplots()
+
+#for chan in range(len(tempLevels)):
+#    x = [int(delay)*int(item) for item in range(len(tempLevels["Channel "+str(chan)]))]
+#    y = tempLevels["Channel "+str(chan)]
+#    allChanAxes[0].plot(x, y, '.', linestyle='dashdot', label="Channel "+str(chan))
+#    allChanAxes[0].set_title("BTL Temperature vs Time")
+#    allChanAxes[1].set_xlabel("Time (Seconds)")
+#    allChanAxes[0].set_ylabel("Temperature ˚C")
+#    allChanFig.savefig(outputName+"/"+outputName+"_ch"+str(chan)+".png", bbox_inches="tight")
+#if args.channel is not None:
+#    print("not none")
+#    x = [int(delay)*int(item) for item in range(len(tempLevels["Channel "+str(args.channel)]))]
+#    y = tempLevels["Channel "+str(args.channel)]
+#    singleChanAxes.title("BTL Temperature vs Time Channel "+str(args.channel))
+#    singleChanAxes.xlabel("Time (Seconds)")
+#    singleChanAxes.ylabel("Temperature ˚C")
+#    singleChanAxes.plot(x, y, '.', linestyle='dashdot', label="Channel "+str(args.channel))
+#    singleChanFig.savefig(outputName+"/"+outputName+"_SINGLE_ch"+str(chan)+".png", bbox_inches="tight")
+#plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+#plt.tight_layout()
+#plt.show()
 
 
 
